29d757931081a197b98d4b7fbe0c2ed7d280cf1e
Andrer50 <sergiorupay990@gmail.com>
2026-04-24 12:29:41 -0500

    configuracion main

---
+++ src/main/java/com/utp/adoptappbackend/AdoptAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.utp.adoptappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdoptAppBackendApplication.class, args);
+    }
+
+}
--- src/test/java/com/utp/adoptappbackend/AdoptAppBackendApplicationTests.java
+++ src/test/java/com/utp/adoptappbackend/AdoptAppBackendApplicationTests.java
@@ -1,13 +1,13 @@
-package com.adoptapp.backend;
+package com.utp.adoptappbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class AdoptAppBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
